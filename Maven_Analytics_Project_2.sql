9e801f5c1f3f025582bdff3a4486a0e9454b64d9
TittaKuusela <111754066+TittaKuusela@users.noreply.github.com>
2022-09-08 14:06:58 +0300

    Update Maven_Analytics_Project_2.sql

--- Maven_Analytics_Project_2.sql
+++ Maven_Analytics_Project_2.sql
@@ -6,33 +6,33 @@ of each property (street address, district, city, and country please).
 
 SELECT 
 	staff.first_name AS manager_first_name, 
-    staff.last_name AS manager_last_name,
-    address.address, 
-    address.district, 
-    city.city, 
-    country.country
+    	staff.last_name AS manager_last_name,
+    	address.address, 
+    	address.district, 
+    	city.city, 
+    	country.country
 
 FROM store
 	LEFT JOIN staff ON store.manager_staff_id = staff.staff_id
-    LEFT JOIN address ON store.address_id = address.address_id
-    LEFT JOIN city ON address.city_id = city.city_id
-    LEFT JOIN country ON city.country_id = country.country_id
+    	LEFT JOIN address ON store.address_id = address.address_id
+    	LEFT JOIN city ON address.city_id = city.city_id
+    	LEFT JOIN country ON city.country_id = country.country_id
 ;
 
 	
 /*
-2.	I would like to get a better understanding of all of the inventory that would come along with the business. 
+2. I would like to get a better understanding of all of the inventory that would come along with the business. 
 Please pull together a list of each inventory item you have stocked, including the store_id number, 
 the inventory_id, the name of the film, the film’s rating, its rental rate and replacement cost. 
 */
 
 SELECT 
 	inventory.store_id, 
-    inventory.inventory_id, 
-    film.title, 
-    film.rating, 
-    film.rental_rate, 
-    film.replacement_cost
+    	inventory.inventory_id, 
+    	film.title, 
+    	film.rating, 
+    	film.rental_rate, 
+    	film.replacement_cost
 FROM inventory
 	LEFT JOIN film
 		ON inventory.film_id = film.film_id
@@ -40,20 +40,20 @@ FROM inventory
 
 
 /* 
-3.	From the same list of films you just pulled, please roll that data up and provide a summary level overview 
+3. From the same list of films you just pulled, please roll that data up and provide a summary level overview 
 of your inventory. We would like to know how many inventory items you have with each rating at each store. 
 */
 
 SELECT 
 	inventory.store_id, 
-    film.rating, 
-    COUNT(inventory_id) AS inventory_items
+    	film.rating, 
+    	COUNT(inventory_id) AS inventory_items
 FROM inventory
 	LEFT JOIN film
 		ON inventory.film_id = film.film_id
 GROUP BY 
 	inventory.store_id,
-    film.rating
+	film.rating
 ;
 
 
@@ -66,10 +66,10 @@ sliced by store and film category.
 
 SELECT 
 	store_id, 
-    category.name AS category, 
+    	category.name AS category, 
 	COUNT(inventory.inventory_id) AS films, 
-    AVG(film.replacement_cost) AS avg_replacement_cost, 
-    SUM(film.replacement_cost) AS total_replacement_cost
+    	AVG(film.replacement_cost) AS avg_replacement_cost, 
+    	SUM(film.replacement_cost) AS total_replacement_cost
     
 FROM inventory
 	LEFT JOIN film
@@ -81,7 +81,7 @@ FROM inventory
 
 GROUP BY 
 	store_id, 
-    category.name
+    	category.name
     
 ORDER BY 
 	SUM(film.replacement_cost) DESC
@@ -89,19 +89,19 @@ ORDER BY
 
 
 /*
-5.	We want to make sure you folks have a good handle on who your customers are. Please provide a list 
+5. We want to make sure you folks have a good handle on who your customers are. Please provide a list 
 of all customer names, which store they go to, whether or not they are currently active, 
 and their full addresses – street address, city, and country. 
 */
 
 SELECT 
 	customer.first_name, 
-    customer.last_name, 
-    customer.store_id,
-    customer.active, 
-    address.address, 
-    city.city, 
-    country.country
+    	customer.last_name, 
+    	customer.store_id,
+    	customer.active, 
+    	address.address, 
+    	city.city, 
+    	country.country
 
 FROM customer
 	LEFT JOIN address ON customer.address_id = address.address_id
@@ -111,7 +111,7 @@ FROM customer
 
 
 /*
-6.	We would like to understand how much your customers are spending with you, and also to know 
+6. We would like to understand how much your customers are spending with you, and also to know 
 who your most valuable customers are. Please pull together a list of customer names, their total 
 lifetime rentals, and the sum of all payments you have collected from them. It would be great to 
 see this ordered on total lifetime value, with the most valuable customers at the top of the list. 
@@ -119,17 +119,17 @@ see this ordered on total lifetime value, with the most valuable customers at th
 
 SELECT 
 	customer.first_name, 
-    customer.last_name, 
-    COUNT(rental.rental_id) AS total_rentals, 
-    SUM(payment.amount) AS total_payment_amount
+    	customer.last_name, 
+    	COUNT(rental.rental_id) AS total_rentals, 
+    	SUM(payment.amount) AS total_payment_amount
 
 FROM customer
 	LEFT JOIN rental ON customer.customer_id = rental.customer_id
-    LEFT JOIN payment ON rental.rental_id = payment.rental_id
+    	LEFT JOIN payment ON rental.rental_id = payment.rental_id
 
 GROUP BY 
 	customer.first_name,
-    customer.last_name
+    	customer.last_name
 
 ORDER BY 
 	SUM(payment.amount) DESC
@@ -145,18 +145,18 @@ it would be good to include which company they work with.
 
 SELECT
 	'investor' AS type, 
-    first_name, 
-    last_name, 
-    company_name
+    	first_name, 
+    	last_name, 
+    	company_name
 FROM investor
 
 UNION 
 
 SELECT 
 	'advisor' AS type, 
-    first_name, 
-    last_name, 
-    NULL
+    	first_name, 
+    	last_name, 
+    	NULL
 FROM advisor;
 
 
@@ -170,10 +170,10 @@ Finally, how about actors with just one award?
 SELECT
 	CASE 
 		WHEN actor_award.awards = 'Emmy, Oscar, Tony ' THEN '3 awards'
-        WHEN actor_award.awards IN ('Emmy, Oscar','Emmy, Tony', 'Oscar, Tony') THEN '2 awards'
+        	WHEN actor_award.awards IN ('Emmy, Oscar','Emmy, Tony', 'Oscar, Tony') THEN '2 awards'
 		ELSE '1 award'
 	END AS number_of_awards, 
-    AVG(CASE WHEN actor_award.actor_id IS NULL THEN 0 ELSE 1 END) AS pct_w_one_film
+	AVG(CASE WHEN actor_award.actor_id IS NULL THEN 0 ELSE 1 END) AS pct_w_one_film
 	
 FROM actor_award
 	
@@ -181,7 +181,7 @@ FROM actor_award
 GROUP BY 
 	CASE 
 		WHEN actor_award.awards = 'Emmy, Oscar, Tony ' THEN '3 awards'
-        WHEN actor_award.awards IN ('Emmy, Oscar','Emmy, Tony', 'Oscar, Tony') THEN '2 awards'
+        	WHEN actor_award.awards IN ('Emmy, Oscar','Emmy, Tony', 'Oscar, Tony') THEN '2 awards'
 		ELSE '1 award'
 	END
 
